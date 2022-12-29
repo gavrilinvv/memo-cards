fbef853012771092fe0a472697ea6c450bb7bb51
Slava <gavrilin2410@gmail.com>
2022-12-29 22:16:23 +0300

    add 404 page

---
+++ 404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/dest/css/style.css?<?=date('Hdmy')?>">
+	<title>404</title>
+</head>
+<body>
+	<div class="not-found__wrapper">
+		<div class="not-found">
+			<div class="not-found__bg">
+				<span class="not-found__code">404</span>
+				<span class="not-found__text">Страница не&nbsp;найдена</a></span>
+			</div>
+			<br>
+			<div class="button_wrap">
+				<a class="button" href="/">На главную</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
